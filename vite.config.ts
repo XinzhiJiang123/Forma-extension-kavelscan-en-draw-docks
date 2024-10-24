4304085f83e311f188103329a257bf29fd0d0b8b
XinzhiJiang <68220328+XinzhiJiang123@users.noreply.github.com>
2024-10-24 09:10:15 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import preact from '@preact/preset-vite';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [preact()],
-	server: {
-        port: 5173,
-    }
+	// server: {
+    //     port: 5173,
+    // }
+    base: '/Forma-extension-kavelscan-en-draw-docks/',
 });
